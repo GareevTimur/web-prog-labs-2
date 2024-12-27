ac0b4766cd97c52c96fcdf4a81159c593b4d4d9a
Гареев Тимур <0124alin@gmail.com>
2024-12-28 02:53:04 +0700

    Заготовка к третьей лабораторной работе

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, redirect, url_for, render_template
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 @app.route("/")
 @app.route("/index")
@@ -34,6 +36,8 @@ def menu():
         <li><a href = "/lab2">Вторая лабораторная</a></li>
         </ul>
         <ul>
+        <li><a href = "/lab3">Третья лабораторная</a></li>
+        </ul>
 
         <footer>
             &copy; Гареев Тимур, ФБИ-23, 3 курс, 2024
