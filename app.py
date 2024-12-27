fa8f193358fa06929a86533b7baeee00dd5235ba
Гареев Тимур <0124alin@gmail.com>
2024-12-28 02:46:40 +0700

    Рефакторинг: выделение второй лабораторной работы

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, redirect, url_for, render_template
+from lab1 import lab1
+from lab2 import lab2
+
 app = Flask(__name__)
+app.register_blueprint(lab1)
+app.register_blueprint(lab2)
 
 @app.route("/")
 @app.route("/index")
@@ -36,56 +41,3 @@ def menu():
     </body>
 </html>
 """
-
-@app.route("/lab2/example")
-def example():
-    name = 'Тимур'
-    number = 2
-    group = 'ФБИ-23'
-    course = 3
-    fruits = [
-        {'name': 'манго', 'price': 100},
-        {'name': 'арбузы', 'price': 120},
-        {'name': 'бананы', 'price': 140},
-        {'name': 'яблоки', 'price': 80}, 
-        {'name': 'персики', 'price': 96}
-    ]
-    books = [
-        {'author': 'Харлан Элисон','naming':'У меня нет рта, но я должен кричать', 'genre': 'постапокалиптика', 'pages': 10},
-        {'author': 'Кормак Маккарти','naming':'Кровавый меридиан', 'genre': 'Вестерн', 'pages': 480},
-        {'author': 'Дж. М. Кёсемен','naming':'Все грядущие дни', 'genre': 'Научная фантастика', 'pages': 308},
-        {'author': 'Дж. К. Роулинг','naming':'Гарри Поттер и Кубок Огня', 'genre': 'Фэнтези, драма', 'pages': 704},
-        {'author': 'Дрю Карпишин','naming':'Звёздные войны. Старая Республика: Реван', 'genre': 'Научная фантастика', 'pages': 298},
-        {'author': 'Джордж Оруэлл','naming':'Скотный двор', 'genre': 'антиутопия, сатира', 'pages': 256},
-        {'author': 'Джордж Оруэлл','naming':'1984', 'genre': 'антиутопия', 'pages': 320},
-        {'author': 'Тимоти Зан','naming':'Траун', 'genre': 'Героическое зарубежное фэнтези ', 'pages': 480},
-        {'author': 'Стивен Кинг','naming':'Кладбище Домашних животных', 'genre': 'Ужасы', 'pages': 373},
-        {'author': 'Дж.Р.Р.Толкин','naming':'Сильмариллион', 'genre': 'Повесть', 'pages': 365}
-    ]
-    return render_template('lab2/example.html', name=name, number=number, group=group, course=course, fruits=fruits, books=books)
-
-@app.route('/lab2/')
-def lab2():
-    return render_template('lab2/lab2.html')
-
-@app.route('/lab2/creatures')
-def creatures():
-    monsters = [
-        {'name': 'Грифон', 'pic': 'грифон.jpeg', 'description': '''
-            Грифоны — противоречивые существа, одновременно объединяющие Небеса и Землю.
-         '''},
-         {'name': 'Минотавр', 'pic': 'минотавр.jpg', 'description': '''
-            Минота́вр  — по греческому преданию, чудовище с телом человека и головой быка.
-         '''},
-         {'name': 'Циклоп', 'pic': 'циклоп.jpg', 'description': '''
-            Циклопы — одноглазые монстры, в разных версиях божественные существа или либо отдельный народ.
-         '''},
-         {'name': 'Бафомет', 'pic': 'бафомет.jpeg', 'description': '''
-            Бафомет - божество знаний и мудрости у эзотериков и оккультистов.
-         '''},
-         {'name': 'Импундулу', 'pic': 'импундулу.jpeg', 'description': '''
-            Импундулу — птица размером с человека, с мощными крыльями, из кончиков которых
-            вылетают молнии, миф о которой распространен в Южной Африке
-         '''}
-    ]
-    return render_template('lab2/creatures.html', monsters=monsters)
