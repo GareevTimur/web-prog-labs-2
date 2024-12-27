400f3b5e41754e9958af8d3576f9586a1804d1a6
Гареев Тимур <0124alin@gmail.com>
2024-12-28 06:26:58 +0700

    Улучшения авторизации

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, redirect, url_for, render_template
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 @app.route("/")
 @app.route("/index")
@@ -30,13 +32,16 @@ def menu():
 
         <h2>Меню</h2>
         <ul>
-        <li><a href = "/lab1">Первая лабораторная</a></li>
+        <li><a href = "/lab1">1 лабораторная</a></li>
         </ul>
         <ul>
-        <li><a href = "/lab2">Вторая лабораторная</a></li>
+        <li><a href = "/lab2">2 лабораторная</a></li>
         </ul>
         <ul>
-        <li><a href = "/lab3">Третья лабораторная</a></li>
+        <li><a href = "/lab3">3 лабораторная</a></li>
+        </ul>
+        <ul>
+        <li><a href = "/lab4">4 лабораторная</a></li>
         </ul>
 
         <footer>
