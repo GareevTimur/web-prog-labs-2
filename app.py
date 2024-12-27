16808b6249c3631813a553bb60f6698fcc0fe38c
Гареев Тимур <0124alin@gmail.com>
2024-12-27 23:12:20 +0700

    Меню

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
     return """
 <!doctype html>
@@ -15,7 +16,41 @@ def start():
         </header>
 
         <h1>web-сервер на flask</h1>
-        
+
+        <h2>Меню</h2>
+        <ul>
+        <li><a href = "/lab1">Первая лабораторная</a></li>
+        </ul>
+
+        <footer>
+            &copy; Гареев Тимур, ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Гареев Тимур Артурович, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <p>Flask — фреймворк для создания веб-приложений на языке
+        программирования Python, использующий набор инструментов
+        Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+        называемых микрофреймворков — минималистичных каркасов
+        веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+        </p>
+
         <footer>
             &copy; Гареев Тимур, ФБИ-23, 3 курс, 2024
         </footer>
