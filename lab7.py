18b069f02fd97ee860cb5f62b11d6c1271eeb289
Гареев Тимур <0124alin@gmail.com>
2024-12-28 08:20:05 +0700

    Получение информации о фильме/-ах по REST

--- lab7.py
+++ lab7.py
@@ -4,3 +4,15 @@ lab7 = Blueprint('lab7',  __name__ )
 @lab7.route('/lab7/') 
 def main(): 
     return render_template('lab7/index.html') 
+
+@lab7.route('/lab7/rest-api/films/', methods=['POST'])
+def add_film(): 
+    film = request.get_json()
+
+    errors, film = check(film)
+    if errors:
+        return errors, 400
+
+    #films.append(film)  
+    insertFilm(film)
+    return "Success", 201
